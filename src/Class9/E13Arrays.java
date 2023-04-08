5aeb58e3bfba59b6b839d68354eb679423c2b6b2
Thiago Silva <thiagollsilva8@gmail.com>
2023-04-08 15:11:18 -0400

    Class 1

---
+++ src/Class9/E13Arrays.java
@@ -0,0 +1,22 @@
+package Class9;
+
+import java.util.Scanner;
+
+public class E13Arrays {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Please enter int numbers");
+            numbers[i] = input.nextInt();
+        }
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
