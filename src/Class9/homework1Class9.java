0aae6b0a2b6a664667a0fcc0992e53f595a9f9a9
Thiago Silva <thiagollsilva8@gmail.com>
2023-04-10 12:10:57 -0400

    Class 9

---
+++ src/Class9/homework1Class9.java
@@ -0,0 +1,24 @@
+package Class9;
+
+/*
+Create an array of chars and store grades into it: A,B,C,D. Then print a grade B (use 2 different ways of creating an array).
+ */
+public class homework1Class9 {
+
+    public static void main(String[] args) {
+
+        char [] grades = {'A', 'B', 'C', 'D'};
+
+        System.out.println(grades[1]);
+
+        System.out.println();
+
+        char [] grade = new char[4];
+        grade[0] = 'A';
+        grade[1] = 'B';
+        grade[2] = 'C';
+        grade[3] = 'D';
+
+        System.out.println(grade[1]);
+    }
+}
---
+++ src/Class9/homework6Class9.java
@@ -0,0 +1,19 @@
+package Class9;
+
+/*
+Create an array on integers and calculate the sum of all elements in an array
+ */
+public class homework6Class9 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 34, 56, 12, 83};
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            System.out.println(sum);
+        }
+    }
+}
